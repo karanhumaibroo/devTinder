1b6d7fbda379fee0cde96472b37c7b0dadc18beb
karanhumaibroo <adarshchoukseyji07@gmail.com>
2025-05-05 15:58:31 +0530

    multiple route handler

--- src/app.js
+++ src/app.js
@@ -2,24 +2,24 @@ const express= require("express");
 
 const app=express();
 
-//only one http that is given
-app.get("/user",(req,res)=>{
-    res.send("data accesed")
-})
-app.post("/user",(req,res)=>{
-    res.send("data posted")
-})
-app.delete("/user",(req,res)=>{
-    res.send("deleted")
-})
 
-app.put("/user",(req,res)=>{
-    res.send("put")
-})
 //this will run for all http request
-app.use("/user",(req,res)=>{
+app.use("/user",(req,res,next)=>{
+    next();
+},
+[(req,res,next)=>{
+      //  res.send("2nd req")
+      next()
+},
+(req,res,next)=>{
+   // res.send("no 3 extend")
+   next();
+}],
+(req,res)=>{
     res.send("no extend")
-})
+}
+
+)
 
 
 app.listen("8000",()=>{
