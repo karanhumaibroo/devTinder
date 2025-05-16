4f198e9e6d3d95803df2972af4a0042f3344bde8
karanhumaibroo <adarshchoukseyji07@gmail.com>
2025-05-16 23:27:46 +0530

    post and get method

--- src/app.js
+++ src/app.js
@@ -2,11 +2,10 @@ const express = require("express");
 const app=express();
 const connection = require("./config/database");
 const User = require("./model/user");
-app.post("/user",async(req,res)=>{
-    const user=new User({
-       name:"adarsh",
-        email:"ajsjjd@gmail.com",
-    });
+app.use( express.json())
+app.post("/signup",async(req,res)=>{
+    const user=new User(req.body);
+   
     try{
         await user.save();
         res.send("user created");
@@ -16,6 +15,27 @@ app.post("/user",async(req,res)=>{
         res.status(500).send(err);
     }
 })
+//single user
+app.get("/users",async(req,res)=>{
+    const age=req.body.age;
+    try{
+        const users=await User.find({age:age});
+        res.send(users);
+    }
+    catch(err){
+        res.status(500).send(err);
+    }   })
+//all users
+app.get("/feed",async(req,res)=>{
+    
+    try{
+        const users=await User.find({});
+        res.send(users);
+    }
+    catch(err){
+        res.status(500).send(err);
+    }   })
+
 
 connection().then(() => {
     console.log("Database connected");
